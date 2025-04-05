cf9f81651e2f58866391788f0190fdd157b8757e
iTsMaaT <matoulimar1@gmail.com>
2025-04-05 12:23:46 -0400

    add extractor pack configuration for SoundcloudExtractor

---
+++ extractorpack.extconfig.js
@@ -0,0 +1,3 @@
+export const EXTRACTOR_PACK_CLASS = "SoundcloudExtractor";
+export const EXTRACTOR_PACK_EXT_OPTIONS = "SoundcloudExtractorInit";
+export const EXTRACTOR_PACK_TYPES = "/dist/index.d.ts";
